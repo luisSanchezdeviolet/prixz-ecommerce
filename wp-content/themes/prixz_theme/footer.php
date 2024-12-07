c20a32b31cbe775653549c57d44d1e59e008cf03
luis fernando cruz sanchez <cruzsanchezluis@vidataservices.com>
2024-12-06 19:28:37 -0600

    Iniciando las bases del custom theme

---
+++ wp-content/themes/prixz_theme/footer.php
@@ -0,0 +1,15 @@
+<?php
+if(!defined('ABSPATH')) {
+    die();;
+}
+?>
+
+
+<footer class="blog-footer">
+  <p>Todos los derechos reservados | <?php echo date('Y'); ?> Desarrollador por <a href="prixz.com" title="Luis Fernando" target="_blank">Luis Fernando Cruz Sánchez</a></p>
+</footer>
+
+
+    
+  </body>
+</html>
