f2c79f7ae23f18e0f30095b5292782ad3d32e2dc
luis fernando cruz sanchez <cruzsanchezluis@vidataservices.com>
2024-12-06 19:34:43 -0600

    Agregando archivo functions y habilitando imagen destacada a los post

---
+++ wp-content/themes/prixz_theme/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+if(!defined('ABSPATH')) {
+    die();;
+}
+
+if(!function_exists('prixz_setup')) {
+    function prixz_setup() {
+        add_theme_support('post-thumbnails');
+    }
+
+    add_action('after_setup_theme', 'prixz_setup');
+
+}
