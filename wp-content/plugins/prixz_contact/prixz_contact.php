247dbaf35462fe9118214d7fa8b4e3c209a14e4f
Luis Cruz <cruzsanchezluis@vidataservices.com>
2024-12-09 06:03:39 +0000

    refactorizando plugin de contacto y agregando tabla de respuestas de usuarios

--- wp-content/plugins/prixz_contact/prixz_contact.php
+++ wp-content/plugins/prixz_contact/prixz_contact.php
@@ -2,39 +2,67 @@
 
 /*
 Plugin Name: Prixz Form Contact
-Plugin URL: luiscrruzsanch3837@gmail.com
-Description: Este es un plugin para el manejo de formularios
+Plugin URL: https://example.com
+Description: Este es un plugin para el manejo de formularios.
 Version: 1.0.1
 Author: Luis Fernando
-license: GPL
+License: GPL
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: prixz_contact
 */
 
-if(!function_exists('prixz_activar_plugin')) {
-    function prixz_activar_plugin() {
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Evita acceso directo.
+}
+
+class PrixzContactPlugin {
+    private $table_name;
+    private $table_name_respuestas;
+
+    public function __construct() {
+        global $wpdb;
+        $this->table_name = $wpdb->prefix . 'prixz_contact';
+        $this->table_name_respuestas = $wpdb->prefix . 'prixz_contact_respuestas';
+
+        register_activation_hook(__FILE__, [ $this, 'activar' ]);
+        register_deactivation_hook(__FILE__, [ $this, 'desactivar' ]);
+    }
+
+    public function activar() {
         global $wpdb;
 
-        $table_name = $wpdb->prefix . 'prixz_contact';
+        $charset_collate = $wpdb->get_charset_collate();
 
-        $sql = "CREATE TABLE if not exists $table_name(
-            id int not null auto_increment,
-            name varchar(200) NOT NULL,
-            email varchar(100) NOT NULL,
-            message TEXT NOT NULL,
-            primary key(id)
-        );";
-
+        $sql = "CREATE TABLE IF NOT EXISTS $this->table_name (
+            id INT NOT NULL AUTO_INCREMENT,
+            name VARCHAR(200) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            fecha DATE,
+            PRIMARY KEY (id)
+        ) $charset_collate;";
         $wpdb->query($sql);
+        $wpdb->query("ALTER TABLE $table_name ADD INDEX(`email`);");
+
+        $sql_respuestas = "CREATE TABLE IF NOT EXISTS $this->table_name_respuestas (
+            id INT NOT NULL AUTO_INCREMENT,
+            prixz_form_principal_id INT NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            phone VARCHAR(15) NOT NULL,
+            message TEXT NOT NULL,
+            fecha DATE,
+            PRIMARY KEY (id),
+            FOREIGN KEY (prixz_form_principal_id) REFERENCES $this->table_name (id)
+        ) $charset_collate;";
+        $wpdb->query($sql_respuestas);
+        $wpdb->query("ALTER TABLE $table_name_respuestas ADD INDEX(`email`);");
+        $wpdb->query("ALTER TABLE $table_name_respuestas ADD INDEX(`phone`);");
+    }
+
+    public function desactivar() {
+        
     }
 }
 
-if(!function_exists('prixz_desactivar_plugin')) {
-    function prixz_desactivar_plugin() {
-
-    }
-}
-
-
-register_activation_hook(__FILE__, 'prixz_activar_plugin');
-register_deactivation_hook(__FILE__, 'prixz_desactivar_plugin');
+new PrixzContactPlugin();
