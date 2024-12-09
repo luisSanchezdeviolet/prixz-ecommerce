88759e61580ae3268725b7ca8d987078e7d9e7c7
Luis Cruz <cruzsanchezluis@vidataservices.com>
2024-12-09 07:39:45 +0000

    Tabla de lista de formularios: Se construyeron los encabezados aplicando bootstrap

--- wp-content/plugins/prixz_contact/includes/listar-formularios.php
+++ wp-content/plugins/prixz_contact/includes/listar-formularios.php
@@ -6,4 +6,29 @@ if (! defined('ABSPATH')) {
 }
 ?>
 
-<h1>hola</h1>
+<div class="wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <h1 class="wp-heading-inline"><?= get_admin_page_title(); ?></h1>
+            <p class="d-flex justify-content-end">
+                <a href="" class="btn btn-primary"><i class="fas fa-plus "></i> Crear</a>
+            </p>
+            <hr>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>Shortcode</th>
+                            <th>Respuestas</th>
+                            <th>Acciones</th>
+                            
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
