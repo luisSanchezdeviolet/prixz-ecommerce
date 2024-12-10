f262215a793d4c8c8a26ab023be58b4ee65defed
Luis Cruz <cruzsanchezluis@vidataservices.com>
2024-12-10 01:33:47 +0000

    Arreglando insert del contact form plugin prixz

--- wp-content/plugins/prixz_contact/uninstall.php
+++ wp-content/plugins/prixz_contact/uninstall.php
@@ -4,11 +4,9 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
-
 
 class PrixzContactUninstaller {
 
@@ -16,20 +14,29 @@ class PrixzContactUninstaller {
      * Ejecuta el proceso de desinstalación.
      */
     public static function run() {
+        error_log('Ejecutando proceso de desinstalación del plugin.');
         self::deleteDatabaseTables();
     }
-
 
     private static function deleteDatabaseTables() {
         global $wpdb;
 
-
         $tableContact = $wpdb->prefix . 'prixz_contact';
         $tableResponses = $wpdb->prefix . 'prixz_contact_respuestas';
 
-
         $wpdb->query( "DROP TABLE IF EXISTS $tableResponses" );
+        if ( $wpdb->last_error ) {
+            error_log( 'Error eliminando la tabla responses: ' . $wpdb->last_error );
+        } else {
+            error_log( "Tabla $tableResponses eliminada exitosamente." );
+        }
+
         $wpdb->query( "DROP TABLE IF EXISTS $tableContact" );
+        if ( $wpdb->last_error ) {
+            error_log( 'Error eliminando la tabla contact: ' . $wpdb->last_error );
+        } else {
+            error_log( "Tabla $tableContact eliminada exitosamente." );
+        }
     }
 }
 
